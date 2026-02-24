75f651e9c9a899bdeec46aff590a5c1123028ac4
Piyusha <piyusha0406@gmail.com>
2026-02-24 17:02:18 +0530

    added the lowerCase button

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -3,8 +3,14 @@ import { useState } from "react";
 function Textform(props) {  
     const [text, setText] = useState("This is for Testing ");
 
-    const handleOnClick =() =>{
+    const handleUpOnClick =() =>{
         let newtext = text.toUpperCase()
+        setText(newtext)
+        console.log("Button clicked")
+    }
+
+      const handleLpOnClick =() =>{
+        let newtext = text.toLowerCase()
         setText(newtext)
         console.log("Button clicked")
     }
@@ -18,7 +24,8 @@ function Textform(props) {
                 <div className="mb-3"> 
                     <textarea className="form-control" value={text} onChange={handleOnChange} id="textform" rows="8"></textarea>
                 </div>
-                <button type="button" className="btn btn-primary" onClick={handleOnClick}>Convert to Uppercase</button>
+                <button type="button" className="btn btn-primary me-2" onClick={handleUpOnClick}>Convert to Uppercase</button> 
+                <button type="button" className="btn btn-primary" onClick={handleLpOnClick}>Convert to Lowercase</button>
             </form>
         )
     

419a8aeebb8e04d379bb4f9ba44a4730af393bf1
Piyusha <piyusha0406@gmail.com>
2026-02-26 14:38:46 +0530

    Added the word and character count below text

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -19,14 +19,23 @@ function Textform(props) {
         setText(event.target.value);
     }
         return (
-            <form>
+            <>
+            <div className="container">
                 <h3>{props.heading}</h3>
                 <div className="mb-3"> 
                     <textarea className="form-control" value={text} onChange={handleOnChange} id="textform" rows="8"></textarea>
                 </div>
                 <button type="button" className="btn btn-primary me-2" onClick={handleUpOnClick}>Convert to Uppercase</button> 
                 <button type="button" className="btn btn-primary" onClick={handleLpOnClick}>Convert to Lowercase</button>
-            </form>
+           </div>
+           <div className="container">
+            <h4>Your Text Summary</h4>
+            <p>{text.split(" ").length} words and {text.length} Characters</p>
+            <p>{0.008 *text.split(" ").length } Minutes read</p>   { /* 1/125 = 0.008 i.e is 1 minute divide by 125 words*/ }
+            <h4>Preview</h4>
+            <p>{text}</p>
+           </div>
+            </>
         )
     
 }
